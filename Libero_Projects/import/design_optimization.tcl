3b7a2f3633e4061ed50e6150dd4f2f23cd064987
Sebastian Slowikowski <Seb.Slowikowski@microchip.com>
2023-06-27 11:15:47 +0100

    Constraint file fixes

--- Libero_Projects/import/design_optimization.tcl
+++ Libero_Projects/import/design_optimization.tcl
@@ -14,26 +14,23 @@ set_root $sdName
 #}
 
 # Import constraint files for all base and design guide configurations
-import_files -sdc $scriptDir/import/constraints/io_sys_constraints.sdc
 import_files -sdc $scriptDir/import/constraints/io_jtag_constraints.sdc
 import_files -io_pdc $scriptDir/import/constraints/io/io_constraints.pdc
 
 # Organize PDC and SDC constraints to Synthesis, Place and Route and Verify Timing tools
+
 # CFG1, CFG2, CFG3 MIV_RV32: Base Configs 
 organize_tool_files -tool {PLACEROUTE} \
 	-file $projectDir/constraint/io/io_constraints.pdc \
 	-file $projectDir/constraint/io_jtag_constraints.sdc \
-	-file $projectDir/constraint/io_sys_constraints.sdc \
 	-module ${sdName}::work -input_type {constraint}
 
 organize_tool_files -tool {SYNTHESIZE} \
 	-file $projectDir/constraint/io_jtag_constraints.sdc \
-	-file $projectDir/constraint/io_sys_constraints.sdc \
 	-module ${sdName}::work -input_type {constraint}
 
 organize_tool_files -tool {VERIFYTIMING} \
 	-file $projectDir/constraint/io_jtag_constraints.sdc \
-	-file $projectDir/constraint/io_sys_constraints.sdc \
 	-module ${sdName}::work -input_type {constraint}
 
 run_tool -name {CONSTRAINT_MANAGEMENT}
